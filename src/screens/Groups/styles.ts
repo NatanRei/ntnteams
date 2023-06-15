ab5ba5450bfcc3d543fbc8bc1be22bb9a16d6dbb
NatanRei <nreischmura@gmail.com>
2023-06-14 21:44:15 -0300

    feat: adjusts ion style

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,8 +3,8 @@ import styled from "styled-components/native";
 export const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.colors.gray_700};
-    /* align-items: center;
-    justify-content: center; */
+
+    padding: 24px;
 `;
 
 export const Title = styled.Text`
